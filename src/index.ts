c25d4f555fc3f175038676292df0659161e4a0c1
RequireSun <kelvinsen@outlook.com>
2021-04-19 02:53:42 +0800

    chore: 启动试试

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export = async function LibDTSLoader() {};
+import * as path from 'path';
+import { bundle } from 'dts-bundle';
+
+bundle({
+  name: 'fxxk',
+  out: path.resolve(__dirname, 'dist'),
+  main: path.resolve(__dirname, '../node_modules/@types/lodash/index.d.ts'),
+});
+
+// export = async function LibDTSLoader() {
+//
+// };
