511e524aaea90362d2f9a938705d676d142dcb02
RequireSun <kelvinsen@outlook.com>
2021-05-06 11:05:53 +0800

    chore: 增加 lint & prettier

--- src/__tests__/cases/basic.ts
+++ src/__tests__/cases/basic.ts
@@ -1,7 +1,8 @@
 // 用法说明: https://webpack.docschina.org/concepts/loaders/#inline
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore 因为 ts 不允许引用 dts 文件, 所以只能写一个 ignore 来强行引入
 import loaderUtils from '!lib-dts-loader!@types/loader-utils/index.d.ts';
 
 export default {
-    loaderUtils,
+  loaderUtils,
 };
--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ async function loader(dtsIndexPath: string) {
 // TODO
 //   这种写法有个小问题, 就是会把 dts bundle 全文生成成一个字符串直接打到代码包里
 //   使用者需要额外编写分包逻辑
-export default function (_source) {
+export default function (/* source: string */) {
   const callback = this.async();
   const parts = getCurrentRequest(this).split('!');
   loader(parts[parts.length - 1]).then(code => callback(null, code));
